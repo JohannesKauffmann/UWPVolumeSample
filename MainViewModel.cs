b31ddfc07ee5819e7a05c3203db06d6e0c314b2d
JohannesKauffmann <19662702+JohannesKauffmann@users.noreply.github.com>
2021-01-08 20:46:56 +0100

    Added button to test if mute "registers" volume, thread to confirm volume can be read after button press, thread to write to volume untill successfull and measure timespan

--- MainViewModel.cs
+++ MainViewModel.cs
@@ -1,6 +1,7 @@
-﻿using System;
+using System;
 using System.ComponentModel;
 using System.Diagnostics;
+using System.Threading;
 using System.Windows.Input;
 using LibVLCSharp.Platforms.UWP;
 using LibVLCSharp.Shared;
@@ -23,6 +24,7 @@ namespace LibVLCSharp.UWP.Sample
         public MainViewModel()
         {
             InitializedCommand = new RelayCommand<InitializedEventArgs>(Initialize);
+            MuteUnmuteCommand = new RelayCommand<EventArgs>(MuteUnmute);
         }
 
         /// <summary>
@@ -37,6 +39,13 @@ namespace LibVLCSharp.UWP.Sample
         /// Gets the command for the initialization
         /// </summary>
         public ICommand InitializedCommand { get; }
+        public ICommand MuteUnmuteCommand { get; }
+
+        private void MuteUnmute(EventArgs e)
+        {
+            MediaPlayer.ToggleMute();
+            //MediaPlayer.Volume = 100; // By the time user is able to activate button, both a write or mute will do the trick
+        }
 
         private LibVLC LibVLC { get; set; }
 
@@ -61,25 +70,65 @@ namespace LibVLCSharp.UWP.Sample
 
         private void Initialize(InitializedEventArgs eventArgs)
         {
+            // Couldn't get this to work
+            //string[] args = new string[3];
+            //args[0] = eventArgs.SwapChainOptions[0];
+            //args[1] = eventArgs.SwapChainOptions[1];
+            //args[2] = "--no-volume-save";
+            //LibVLC = new LibVLC(enableDebugLogs: false, args);
+
             LibVLC = new LibVLC(enableDebugLogs: false, eventArgs.SwapChainOptions);
+
             MediaPlayer = new MediaPlayer(LibVLC);
             MediaPlayer.Playing += MediaPlayer_Playing;
-            MediaPlayer.VolumeChanged += MediaPlayer_VolumeChanged;
-            Debug.WriteLine("Volume after creating mediaplayer: " + MediaPlayer.Volume);
-            Debug.WriteLine("Setting volume to 20 percent");
-            MediaPlayer.Volume = 20;
+            MediaPlayer.VolumeChanged += (sender, e) => Debug.WriteLine("Volume changed! New volume: " + e.Volume);
+            MediaPlayer.Muted += (sender, args) => Debug.WriteLine("Muted!");
+            MediaPlayer.Unmuted += (sender, args) => Debug.WriteLine("Unmuted!");
+
+            Debug.WriteLine("Volume after creating mediaplayer: " + MediaPlayer.Volume); // Is always 0
+            //Debug.WriteLine("Setting volume to 20 percent");
+            //MediaPlayer.Volume = 20; // Never works
+
+            Thread queryThread = new Thread(new ThreadStart(Query));
+            queryThread.Start();
+
+            // Uncomment setThread.Start() to print timespan between creating mediaplayer and first time the property can and has been written to, usually 600-700 ms
+            // Otherwise, print timespan between creating mediaplayer and the volume "registering" as a result from pressing the button
+            Thread setThread = new Thread(new ThreadStart(SetVolume));
+            //setThread.Start();
+
             using var media = new Media(LibVLC, new Uri("http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4"));
             MediaPlayer.Play(media);
         }
 
-        private void MediaPlayer_VolumeChanged(object sender, MediaPlayerVolumeChangedEventArgs e)
+        bool flag = false;
+
+        private void Query()
         {
-            Debug.WriteLine("Volume changed! New volume: " + e.Volume);
+            DateTime start = DateTime.Now;
+            DateTime not0 = DateTime.Now;
+            while (MediaPlayer.Volume == 0)
+            {
+                not0 = DateTime.Now;
+            }
+            flag = true;
+            Debug.WriteLine("Volume took " + not0.Subtract(start).TotalSeconds/*ToString(@"ss\.fff")*/ + " seconds since creating mediaplayer to register as non-zero");
+        }
+
+        private void SetVolume()
+        {
+            while (!flag)
+            {
+                MediaPlayer.Volume = 100;
+            }
         }
 
         private void MediaPlayer_Playing(object sender, EventArgs e)
         {
-            Debug.WriteLine("Playing event! Volume: " + MediaPlayer.Volume);
+            ThreadPool.QueueUserWorkItem(_ =>
+            {
+                Debug.WriteLine("Playing event! Volume: " + MediaPlayer.Volume); // Is always 0
+            });
         }
 
         /// <summary>
--- README.md
+++ README.md
@@ -3,7 +3,6 @@ Volume issue repro for UWP
 
 This small sample will:
 - Read the volume after creating a media player
-- Set the volume to 20 percent
-- Read the volume again after the video has started playing
-
-This doesn't work as expected :(
+- Query the volume on another thread untill it is non-zero and print elapsed time
+- Depending on line 98 from MainViewModel.cs, try to write to the volume on another thread untill it can be successfully read back on the other thread
+- Mute and unmute the player when hitting the button. This will "register" the volume to it's actual value
